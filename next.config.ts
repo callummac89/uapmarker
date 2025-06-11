f9e668d870775f94c8515760d74c3fc722e5452b
Callum MacInnes <callum.macinnes89@gmail.com>
2025-06-11 12:14:55 +0100

    Fixed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    webpack(config) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    webpack(config: any, _context: any) {
         config.module.rules.push({
             test: /\.geojson$/,
             type: 'json',

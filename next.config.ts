f1c0027cda6350f4f1249c3d919c49b01f143dd4
Callum MacInnes <callum.macinnes89@gmail.com>
2025-06-13 21:59:23 +0100

    PWA

--- next.config.ts
+++ next.config.ts
@@ -19,3 +19,14 @@ module.exports = nextConfig;
 
 export default nextConfig;
 
+// next.config.js
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+    reactStrictMode: true,
+});
